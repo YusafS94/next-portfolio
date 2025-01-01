2809e4d440b2aa83056b896ce74ce5c8be59e534
YusafS94 <yusafsaddiq@gmail.com>
2025-01-01 19:31:46 +0000

    professional, personal and hobbies tabs now loading in as strings from developer config about object

--- app/portfolio/about-me/page.tsx
+++ app/portfolio/about-me/page.tsx
@@ -1,6 +1,25 @@
 import AboutContent from "@/app/ui/portfolio/about-me/about-content";
 import { redirect } from "next/navigation";
+import developerConfig from "@/app/lib/developer.json";
 export default function Page() {
   // redirect("/portfolio/about-me/professional-info");
-  return <></>;
+  // console.log(Object.keys(developerConfig));
+  const tabs = Object.keys(developerConfig.about.sections);
+  console.log(tabs);
+  console.log(tabs[0]);
+  tabs.map((tab) => {
+    <p>{tab}</p>;
+  });
+
+  return (
+    <>
+      <ul className="flex flex-col">
+        {tabs.map((tab) => (
+          <li key={tab} style={{ marginBottom: "16px" }}>
+            {tab}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
