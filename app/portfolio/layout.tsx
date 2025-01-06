45473c0d632c3c45990ab41c412cda1f187754f6
YusafS94 <yusafsaddiq@gmail.com>
2025-01-06 13:11:55 +0000

    project page responsivity and overflow scroll done

--- app/portfolio/layout.tsx
+++ app/portfolio/layout.tsx
@@ -6,10 +6,10 @@ import Footer from "../ui/portfolio/footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className={`${fira_code.className} h-screen flex-col md:overflow-hidden bg-slate-950`}
+      className={`${fira_code.className} h-full md:h-screen flex-col md:overflow-hidden bg-slate-950`}
     >
-      <div className="h-screen p-4">
-        <div className="border border-lines-1 rounded-lg h-full bg-primary-2 text-secondary-1 flex flex-col">
+      <div className="md:h-screen p-4">
+        <div className="border border-lines-1 rounded-lg h-auto md:h-full bg-primary-2 text-secondary-1 flex flex-col">
           <nav className="topnav flex-none">
             <TopNav />
           </nav>
--- app/ui/portfolio/projects/Project.tsx
+++ app/ui/portfolio/projects/Project.tsx
@@ -1,21 +1,55 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Project( {id, title, content, date, tags}: { id: string, title: string, content: string, date: string, tags: string[]}) {
-    return (
-        <div key={id} className="py-4 px-24 overflow-scroll overflow-x-hidden h-[500px] border border-red-500 w-full">
-            <Link href={`/portfolio/projects`}>Back</Link>
-            <h1 className="text-3xl text-center">{title}</h1>
-            <div className="border rounded-lg my-2">
+export default function Project({
+  id,
+  title,
+  content,
+  date,
+  tags,
+}: {
+  id: string;
+  title: string;
+  content: string;
+  date: string;
+  tags: string[];
+}) {
+  return (
+    <div
+      key={id}
+      className="py-4 px-[10vw] overflow-scroll overflow-x-hidden max-h-[100vh] border border-red-500 w-full"
+    >
+      <Link href={`/portfolio/projects`}>Back</Link>
+      <div className="flex flex-col md:flex-row max-h-[90vh] md:max-h-[75vh]">
+          <div className="w-full md:w-1/2">
+              <h1 className="text-3xl text-center">{title}</h1>
+              <div className="border rounded-lg my-2">
                 <p className="text-gray-500">{date}</p>
                 <p>{content}</p>
                 <ul className="list-disc ml-4">
-                    {tags.map((tag) => (
-                        <li key={tag}>{tag}</li>
-                    ))}
+                  {tags.map((tag) => (
+                    <li key={tag}>{tag}</li>
+                  ))}
                 </ul>
-            </div>
-            <Image className="rounded-lg overflow-hidden self-center" src="/images/project-placeholder.jpg" alt="Placeholder image" width={700} height={500} />
-        </div>
-    )
+              </div>
+          </div>
+          <div className="w-full md:w-1/2">
+              <Image
+                className="rounded-lg overflow-hidden self-center"
+                src="/images/project-placeholder.jpg"
+                alt="Placeholder image"
+                width={800}
+                height={800}
+              />
+              <Image
+                className="rounded-lg overflow-hidden self-center"
+                src="/images/project-placeholder.jpg"
+                alt="Placeholder image"
+                width={800}
+                height={800}
+              />
+          </div>
+      </div>
+    </div>
+  );
 }
