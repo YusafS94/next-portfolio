2ba9f1234bf20a40a8a41f4671c0d9b84fe8ce05
YusafS94 <yusafsaddiq@gmail.com>
2025-01-06 00:22:31 +0000

    project page scroll working and custom scrollbar added

--- app/ui/portfolio/projects/Project.tsx
+++ app/ui/portfolio/projects/Project.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 
 export default function Project( {id, title, content, date, tags}: { id: string, title: string, content: string, date: string, tags: string[]}) {
     return (
-        <div key={id} className="p-4 overflow-hidden">
-            <Link href={`/portfolio/projects/project/${id}`}><h1>{title}</h1></Link>
-            <p className="text-gray-500">{date}</p>
-            <p>{content}</p>
-            <ul className="list-disc ml-4">
-                {tags.map((tag) => (
-                    <li key={tag}>{tag}</li>
-                ))}
-            </ul>
+        <div key={id} className="py-4 px-24 overflow-scroll overflow-x-hidden h-[500px] border border-red-500 w-full">
             <Link href={`/portfolio/projects`}>Back</Link>
-            <Image className="rounded-lg overflow-hidden" src="/images/project-placeholder.jpg" alt="Placeholder image" width={700} height={500} />
+            <h1 className="text-3xl text-center">{title}</h1>
+            <div className="border rounded-lg my-2">
+                <p className="text-gray-500">{date}</p>
+                <p>{content}</p>
+                <ul className="list-disc ml-4">
+                    {tags.map((tag) => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
+            </div>
+            <Image className="rounded-lg overflow-hidden self-center" src="/images/project-placeholder.jpg" alt="Placeholder image" width={700} height={500} />
         </div>
     )
 }
