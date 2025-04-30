550cac4d60e3e147c53caf68978ae42f503d9f4f
YusafS94 <yusafsaddiq@gmail.com>
2025-04-30 09:37:20 +0100

    file cleanup, unused/old files removed

--- app/portfolio/about-me/hobbies-info/page.tsx
+++
@@ -1,9 +0,0 @@
-import config from "@/app/lib/developer.json";
-import AboutHobTabs from "@/app/ui/portfolio/about-me/about-hobbies-tabs";
-export default function Page() {
-  return (
-    <>
-      <AboutHobTabs />
-    </>
-  );
-}
--- app/portfolio/about-me/personal-info/page.tsx
+++
@@ -1,9 +0,0 @@
-import config from "@/app/lib/developer.json";
-import AboutPersTabs from "@/app/ui/portfolio/about-me/about-personal-tabs";
-export default function Page() {
-  return (
-    <>
-      <AboutPersTabs />
-    </>
-  );
-}
