b291ef11cc689b76698a14004f11b683b2d1462f
YusafS94 <yusafsaddiq@gmail.com>
2025-05-22 17:36:25 +0100

    font sizes added to tailwind config file and flex styling reset on homepage

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,11 @@
 export default function Page() {
   return (
-    <div className="flex flex-col md:flex-row justify-between w-8/12 m-auto">
-      <div className="flex flex-col justify-between hero-text w-full md:w-1/2">
+    <div className="">
+      <div className="">
         <div className="hero-top-text text-secondary-4">
-          <p>Hi, I'm</p>
+          <p className="text-base">Hi, I'm</p>
           <h1 className="text-6xl">Yusaf Saddiq</h1>
-          <p className="text-lg text-secondary-3"> _Web Developer</p>
+          <p className="text-4xl text-secondary-3"> _Web Developer</p>
         </div>
         <div className="hero-bottom-text">
           <p>// front-end developer</p>
@@ -14,14 +14,18 @@ export default function Page() {
             <span className="text-secondary-3">const</span>
             &nbsp;
             <span className="text-accent-2">githubLink</span>
-            &nbsp;
-            =
-            &nbsp;
+            &nbsp; = &nbsp;
           </p>
-          <a className="text-accent-1" href="https://github.com/YusafS94">https://github.com/YusafS94</a>
+          <a
+            className="text-accent-1"
+            target="_blank"
+            href="https://github.com/YusafS94"
+          >
+            https://github.com/YusafS94
+          </a>
         </div>
       </div>
-      <div className="hidden md:block hero-right text-secondary-4 bg-gradient-to-b from-cyan-500 to-slate-900 w-full md:w-1/2 rounded-lg p-6">
+      <div className="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem tenetur
         eligendi laboriosam, cupiditate vel error porro eum alias modi aliquid
         suscipit iusto, consequuntur quisquam debitis blanditiis repellendus
