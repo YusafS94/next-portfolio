ecfe7c8d226c562431ca1a54468ea7855a7d10ee
YusafS94 <yusafsaddiq@gmail.com>
2025-06-01 22:16:18 +0100

    unused files and folders cleanup from dashboard tutorial starter code and hamburger menu tests

---
+++ app/lib/project-data.ts
@@ -0,0 +1,52 @@
+const projects = [
+  {
+    id: "1",
+    title: "Saddiq Hotels",
+    content:
+      "Lorem ipsum dolor sit amet, consecutur adipiscing elit. Sed do ei",
+    date: "01-01-2024",
+    tags: ["php", "sql"],
+    image1: "hero-desktop",
+    image2: "hero-desktop",
+    image3: "hero-desktop",
+    image4: "hero-desktop",
+  },
+  {
+    id: "2",
+    title: "FighterTrax",
+    content:
+      "Lorem ipsum dolor sit amet, consecutur adipiscing elit. Sed do ei",
+    date: "02-01-2024",
+    tags: ["react", "tailwind"],
+    image1: "desktop-dashboard",
+    image2: "hero-desktop",
+    image3: "hero-desktop",
+    image4: "hero-desktop",
+  },
+  {
+    id: "3",
+    title: "Skate It Yourself",
+    content:
+      "Lorem ipsum dolor sit amet, consecutur adipiscing elit. Sed do ei",
+    date: "03-01-2024",
+    tags: ["wordpress", "custom-theme"],
+    image1: "hero-desktop",
+    image2: "hero-desktop",
+    image3: "hero-desktop",
+    image4: "hero-desktop",
+  },
+  {
+    id: "4",
+    title: "Bury Defence Academy",
+    content:
+      "Lorem ipsum dolor sit amet, consecutur adipiscing elit. Sed do ei",
+    date: "04-01-2024",
+    tags: ["react", "next.js", "tailwind"],
+    image1: "hero-desktop",
+    image2: "hero-desktop",
+    image3: "hero-desktop",
+    image4: "hero-desktop",
+  },
+];
+
+export { projects };
